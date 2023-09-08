efc248754403659d544e9e5a051a25f5d8773679
valroy <valerie.roy@mines-paristech.fr>
2023-09-08 08:34:01 +0200

    correction of a markdown array

--- notebooks/.teacher/1-02-numpy-array-corrige-nb.py
+++ notebooks/.teacher/1-02-numpy-array-corrige-nb.py
@@ -374,6 +374,8 @@ matrice = [
     [17, 5, 127]
 ]
 mat = np.array(matrice, dtype=np.uint8)
+# note that new version of NumPy "will stop allowing conversion of out-of-bound Python integers to integer arrays.
+# The conversion of -128 to uint8 will fail in the future."
 mat
 
 # %% [markdown]
@@ -398,8 +400,10 @@ l = [[  0,   8,  34,   8],
 print(    np.array(l, dtype=np.uint8)    )
 
 print(    np.array(l, dtype=np.int8)    )
+# note that new version of NumPy "will stop allowing conversion of out-of-bound Python integers to integer arrays.
+# The conversion of -128 to uint8 will fail in the future."
 
-# %% [markdown] tags=["framed_cell"] jp-MarkdownHeadingCollapsed=true
+# %% [markdown] tags=["framed_cell"]
 # ### modifier le type des éléments d'un tableau existant
 #
 # ````{admonition} →
@@ -509,14 +513,14 @@ print(    mat*100    )
 # ````{admonition} →
 #
 # | les méthodes | ce qu'elles font |
-# | --------------------------- | --------------------- ---------------------- |
+# | --------------------------- | ------------------------------------------- |
 # | `numpy.zeros` | renvoie un ndarray rempli de *0.* (float) |
 # | `numpy.ones` | renvoie un ndarray rempli de *1.* (float) |
 # | `numpy.empty` | renvoie un ndarray vide i.e. sans initialiser ses éléments |
-#     | | |
+# | | |
 # | `numpy.arange` | tableau de valeurs régulièrement espacées|
 # | `numpy.linspace` |  tableau de valeurs régulièrement espacées|
-#     | | |
+# | | |
 # | `numpy.random.randint` | entiers aléatoirement générés |
 # | `numpy.random.randn` | flottants aléatoirement générés |
 # ````
