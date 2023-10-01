0c697bed5e3d43d5da02b1fb47fb2f752284de0b
valroy <valerie.roy@mines-paristech.fr>
2023-10-01 18:40:43 +0200

    refactoring-p24 of the notebook 2-08-pandas-create-dataframe-corrige-nb.py

--- notebooks/.teacher/2-08-pandas-create-dataframe-corrige-nb.py
+++ notebooks/.teacher/2-08-pandas-create-dataframe-corrige-nb.py
@@ -6,7 +6,7 @@
 #     notebook_metadata_filter: all, -jupytext.text_representation.jupytext_version,
 #       -jupytext.text_representation.format_version,-language_info.version, -language_info.codemirror_mode.version,
 #       -language_info.codemirror_mode,-language_info.file_extension, -language_info.mimetype,
-#       -toc
+#       -toc, -rise, -version
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -27,7 +27,7 @@
 
 # %%
 from IPython.display import HTML
-HTML(url="https://raw.githubusercontent.com/ue12-p23/numerique/main/notebooks/_static/style.html")
+HTML(filename="_static/style.html");
 
 # %% [markdown]
 # # création de dataframe
@@ -78,18 +78,6 @@ import numpy as np
 # ```
 # ````
 
-# %%
-# le code
-import pandas as pd
-import numpy as np
-cols_dict = {'names' : ['snail', 'pig', 'elephant', 'rabbit',
-                        'giraffe', 'coyote', 'horse'],
-             'speed' : [0.1, 17.5, 40, 48, 52, 69, 88],
-             'lifespan' : [2, 8, 70, 1.5, 25, 12, 28], }
-
-df = pd.DataFrame(cols_dict)
-df
-
 # %% [markdown] tags=["framed_cell"]
 # ### à partir du `dict` des colonnes et d'une `list` (d'index) des lignes
 #
@@ -124,16 +112,6 @@ df
 #
 # on peut ne pas lui passer la liste des id des lignes
 # ````
-
-# %%
-cols_dict = {'speed' : [0.1, 17.5, 40, 48, 52, 69, 88],
-             'lifespan' : [2, 8, 70, 1.5, 25, 12, 28], }
-
-line_ids =  ['snail', 'pig', 'elephant', 'rabbit',
-             'giraffe', 'coyote', 'horse']
-
-df = pd.DataFrame(cols_dict, index = line_ids)
-df.values
 
 # %% [markdown] tags=["framed_cell"]
 # ### à partir d'un `numpy.ndarray`
@@ -201,41 +179,10 @@ df.values
 # ```
 # ````
 
-# %%
-# le code
-nd = np.array([[ 0.1,  2. ],
-               [17.5,  8. ],
-               [40. , 70. ],
-               [48. ,  1.5],
-               [52. , 25. ],
-               [69. , 12. ],
-               [88. , 28. ]])
-
-df = pd.DataFrame(nd)
-df
-
-# %%
-# le code
-nd = np.array([[ 0.1,  2. ],
-               [17.5,  8. ],
-               [40. , 70. ],
-               [48. ,  1.5],
-               [52. , 25. ],
-               [69. , 12. ],
-               [88. , 28. ]])
-
-df = pd.DataFrame(nd,
-                  index=['snail', 'pig', 'elephant',
-                         'rabbit', 'giraffe', 'coyote', 'horse'],
-                  columns = ['speed', 'lifespan'])
-df['Names'] = df.index
-df.values
-
-# %% [markdown]
-# ### **exercice** : création de df et type des éléments
-
-# %% [markdown]
-# 1. créer un `numpy.ndarray` à partir de la liste suivante
+# %% [markdown] tags=["framed_cell"]
+# ````{exercise} création de dataframe à partir de liste Python
+#
+# 1. créer la liste `Python` de ces animaux, leur vitesse et leur durée de vie
 # ```python
 # animals = [['snail', 0.1, 2.0],
 #            ['pig', 17.5, 8.0],
@@ -245,108 +192,11 @@ df.values
 #            ['coyote', 69.0, 12.0],
 #            ['horse', 88.0, 28.0]]
 # ```
-# 1. Affichez le type des éléments de la table  
-# Que constatez-vous ? (U = Unicode)
 #
-# 1. Créez une `pandas.DataFrame` à partir de la table précédente  
-# avec pour noms de colonnes `'names'`, `'speed'` et `'lifespan'`
+# 1. créez une `pandas.DataFrame` à partir de la liste précédente  
+# en lui passant en colonnes la liste `['names', 'speed' et 'lifespan']`
 #
-# 1. affichez la valeur et le type du `'lifespan'` de l'éléphant  
-# Que constatez-vous ?  
-# (`object` signifie ici `str`)
+# 1. affichez le type des colonnes
 #
-# 1. affichez la valeur et le type du `'names'` de l'éléphant  
-# Que constatez-vous ?
 #
-# 1. avec `loc` ou `iloc`, modifiez la valeur `elephant` par `'grey elephant'`  
-# affichez la valeur et le type du `'names'` de l'éléphant  
-# un constat ?
-#
-# 1. affichez le type des colonnes  
-# utilisez l'attribut `dtypes` des `pandas.DataFrame`
-#
-# 1. avec la méthode `pandas.DataFrame.to_numpy`  
-# affichez le tableau `numpy` sous-jacent de votre data-frame  
-# affichez le type du tableau  
-# que constatez-vous ?
-#
-# 1. modifiez les colonnes `'speed'` et `'lifespan'` de manière à leur donner le type `float`  
-# (utilisez `pandas.Series.astype` voir les **rappels** en fin de cellule)
-#
-# **rappel**
-#
-# * `astype`  
-# la méthode `pandas.Series.astype`, à laquelle vous indiquez un type `float`  
-# crée (si c'est possible) une nouvelle `pandas.Series` dont les éléments sont de type `float`
-#
-# * rajouter ou modifier une colonne dans une `pandas.DataFrame`  
-# revient à modifier ou rajouter une clé à un `dict`
-#
-# **explication**
-#
-# * quand les types des colonnes `numpy` ne sont pas homogènes  
-# `numpy` met un tableau de caractères `Unicode` de la *plus grande taille*
-#
-# * quand les types des colonnes `pandas` ne sont pas homogènes  
-# sans indication, `pandas` met `str` `Python`
-#
-# * quand dans une data-frame `pandas` on mélange des types de colonnes - genre `float` et `str`  
-# `pandas` et son tableau `numpy` sous-jacent indiqueront `O` ou `object`  
-# pour **mixed data types in columns**
-
-# %%
-# prune-cell
-animals = [['snail', 0.1, 2.0],
-           ['pig', 17.5, 8.0],
-           ['elephant', 40.0, 70.0],
-           ['rabbit', 48.0, 1.5],
-           ['giraffe', 52.0, 25.0],
-           ['coyote', 69.0, 12.0],
-           ['horse', 88.0, 28.0]]
-
-# %%
-# prune-cell 1.
-nd = np.array(animals)
-
-# 2.
-print(nd.dtype)
-
-# %%
-# prune-cell 3.
-df = pd.DataFrame(nd, columns=['names', 'speed', 'lifespan'])
-df
-
-# %%
-# prune-cell 4.
-x = df.loc[2, 'lifespan']
-x, df.lifespan.dtype, type(x)
-
-# %%
-# prune-cell 5.
-x = df.loc[2, 'names']
-x, df.names.dtype, type(x)
-
-# %%
-# prune-cell 6.
-# pas de souci pour mettre une chaine plus longue
-df.loc[2, 'names'] = 'grey elephant grey elephant more than 32'
-df
-
-# %%
-# prune-cell 7.
-df.dtypes
-
-# %%
-# prune-cell 8.
-df.to_numpy()
-
-# %%
-# prune-cell 9.
-df['speed'] = df['speed'].astype(float)
-df['lifespan'] = df['lifespan'].astype(float)
-df.dtypes
-
-# %% [markdown]
-# ***
-
-# %%
+# ````
