abc75e4c4b175948290bac42ce59e7453217a034
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-10-16 17:04:18 +0200

    add a link to the geopandas TP

---
+++ notebooks/.teacher/2-13-geopandas-TP-paris-corrige-nb.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-hidden,-heading_collapsed,-run_control,-trusted
+#     cell_metadata_json: true
+#     notebook_metadata_filter: all, -jupytext.text_representation.jupytext_version,
+#       -jupytext.text_representation.format_version,-language_info.version, -language_info.codemirror_mode.version,
+#       -language_info.codemirror_mode,-language_info.file_extension, -language_info.mimetype,
+#       -toc, -rise, -version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+#   language_info:
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#   nbhosting:
+#     title: TP geopandas
+# ---
+
+# %% [markdown]
+# Licence CC BY-NC-ND, Valérie Roy & Thierry Parmentelat
+
+# %% [markdown]
+# # TP geopandas
+#
+# ````{admonition} un peu de geopandas
+#
+# Voyez le TP qui se trouve ici
+#
+# <https://nbhosting.inria.fr/builds/ds-exos/data-science/latest/ds-tps/geo/README-geopandas-nb.html>
+# ````
